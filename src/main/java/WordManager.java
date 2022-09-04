6eebc7e2dacef9a43668afb784452bf64dfca564
22100451Yejin <22100451@handong.ac.kr>
2022-09-04 18:24:48 +0900

    4번 add() & addWord() 구현

--- src/main/java/WordManager.java
+++ src/main/java/WordManager.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 public class WordManager {
-    Scanner s=new Scanner(System.in);
+    Scanner s =new Scanner(System.in);
+    WordCRUD wordCRUD;
 
-
+    WordManager(){
+        wordCRUD = new WordCRUD(s);
+    }
     public int selectMenu(){
         System.out.print("""
                 *** 영단어 마스터 ***
@@ -24,7 +27,13 @@ public class WordManager {
     public void start(){
         while(true) {
             int menu = selectMenu();
-            if(menu==0) break;
+            if (menu==0) break;
+            if (menu==4){
+                wordCRUD.addWord();
+            }
+            else if(menu==1){
+                //list
+            }
         }
     }
 }
